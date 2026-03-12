d158040fadc6825daa70e057b6a10174f6c13834
MaximusPro <maximusvj@gmail.com>
2026-03-12 22:44:07 +0100

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-define("ROOT", __DIR__. "/");
-
-define('TELEGRAM_TOKEN',    '8247918408:AAGH0WoHN9vM1IyNyBM4Oh50u6Ra6RhU6T8');
-define('TELEGRAM_CHAT_ID',  '5109313351');         // твой chat id
-?>
